75094d885ff23624f170766c76dcb00eb4ad1853
H-Finch-404 <65445401+H-Finch-404@users.noreply.github.com>
2020-12-24 16:51:27 +0100

    Everything pretty much lol

---
+++ addons/XIM_UMC_Integration/config.cpp
@@ -0,0 +1,977 @@
+class CfgMusic
+{
+    class Act_of_War_Military_Strength
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+       class Act_of_War_War_Skills
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+       class COD_Crows_Nest
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class COD_End_Credits
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+      class COD_Evacuation_Crash_Site
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+     class COD_Modern_Combat
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+      class COD_Modern_Combat
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+    class COD_Opening_Credits
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class COD_Rooftops
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+      class COD_Rooftops
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+      class COD_Rooftops
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+      class COD_Spec_Ops
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+      class COD_Their_Own_Accord
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class COD_Whiskey_Hotel_Green_Flares
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class dunkirk_end_titles
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Hitman_Plan_B
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class jack_reacher
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Predator
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Prelude_to_War
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class SOCOM_Assault
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class The_Rock
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class usa_beach_landing
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class usa_Behind_enemy_lines
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class usa_cas
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Warrior_March
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class World_at_War
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class sunrise
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class warlords
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Colombus
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class De_Bello_Civili_Romanum
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Duke_of_Death
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Rome_At_War
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Triarii
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class War_Of_Kings
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Dynasty
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class Black_Hawk_Down_Hes_Dead
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Black_Hawk_Down_Leave_no_Man_Behind
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Crysis_First_Light
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Fury_Airfight
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class Hitman_a_World_Of_Assassination
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class Hitman_Showstopper
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Hold_the_Bridge
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class insurgency
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class Modern_Warfare
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+
+    class Reaper_1_1
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Six_days
+    {
+        moods[] = 
+        {
+            "calm","dark"
+        };
+    };
+
+    class Storm_and_Fury
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Terminator_Theme
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class tribal_oasis
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class cowboys_aliens
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class falling_snow
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class fishing_village
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class Hero_Lorne_Balfe
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class Never_Never_Never_Give_Up
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class Never_Never_Never_Give_Up
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class radioman_song
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class The_Godfather
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class the_misty_mountains_cold
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class airport_stalk
+    {
+        moods[] = 
+        {
+            "intense","dark"
+        };
+    };
+
+    class Black_Cats
+    {
+        name = "Black Cats";
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class date_with_destiny
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class double_agent
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Fury_This_Is_My_Home
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class last_man_standing
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class smoke_and_mirrors
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class stormy_seas
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Supermarine
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class The_Gulag
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class The_Mole
+    {
+        moods[] = 
+        {
+            "intense","dark"
+        };
+    };
+
+    class apocalypse
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class Area_51
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class assassin
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class COD_All_Ghillied_Up
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class covert
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class Crisis
+    {
+        moods[] = 
+        {
+            "dark","calm"
+        };
+    };
+
+    class espionage
+    {
+        moods[] = 
+        {
+            "dark","calm"
+        };
+    };
+
+    class Eye_in_the_Sky
+    {
+        moods[] = 
+        {
+            "dark","intense"
+        };
+    };
+
+    class guerilla
+    {
+        moods[] = 
+        {
+            "dark", "calm"
+        };
+    };
+
+    class Hacker
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class hunter
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class intel
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class Midnight
+    {
+        moods[] = 
+        {
+            "calm","dark"
+        };
+    };
+
+    class ninja
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class Professional
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class silent_assassin
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class sniper
+    {
+        moods[] = 
+        {
+            "dark","calm"
+        };
+    };
+
+    class The_Beast
+    {
+        moods[] = 
+        {
+            "dark","intense"
+        };
+    };
+
+    class The_Coup
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Thunder_Run
+    {
+        moods[] = 
+        {
+            "intense","dark"
+        };
+    };
+
+    class Watcher
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class Wetworks
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class Whiskey_Hotel
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class legendary
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class Moby_Flower
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class rasputin
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class renegades
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class skyfall
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class The_Devil_and_The_Huntsman
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class this_is_where_you_can_reach_me_now
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class wild_thing
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class Resist_And_Bite
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class fortunate_son
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class Paint_It_Black
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Run_Through_The_Jungle
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class war
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class War_Pigs
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class highway_to_hell
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class delirio_mind
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class ride_of_the_valkyries
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Lone_Digger
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class 30minMetal_Gear_Caution
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class battlefield_1942
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Country_Roads
+    {
+        moods[] = 
+        {
+            "calm"
+        };
+    };
+
+    class Fallout_4_The_Commonwealth
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class Seven_Nation_Army_Glitch_Mob_Remix
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class warsaw
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Darth_Vader
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class Darth_Vader
+    {
+        moods[] = 
+        {
+            "dark"
+        };
+    };
+
+    class Grand_Admiral_Thrawn
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class Stronger_Than_Fate
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+    class without_me
+    {
+        moods[] = 
+        {
+            "intense"
+        };
+    };
+
+
+
+
+
+
+
+}; // END OF CFGMUSIC
