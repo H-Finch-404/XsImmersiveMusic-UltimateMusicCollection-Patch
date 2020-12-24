ecb70b38c9d1cd4c415956dc95f112cab9720374
H-Finch-404 <65445401+H-Finch-404@users.noreply.github.com>
2020-12-24 12:34:56 +0100

    Created basic structure and mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,12 @@
+name = "XIM - Ultimate Music Collection Integration";
+picture = "\XIM_Core\XIMCore.paa";
+actionName = "Steam";
+action = "placeholder";
+description = "An expansion patch on XIM to include music featured in Hannes' Ultimate Music Collection (UMC) mod into the dynamic system.";
+logo = "\XIM_Core\XIMCore.paa";
+logoOver = "\XIM_Core\XIMCore.paa";
+tooltip = "XIM - UMC Integration";
+tooltipOwned = "";
+overview = "An expansion patch on XIM to include music featured in Hannes' Ultimate Music Collection (UMC) mod into the dynamic system.";
+author = "H.Finch and x00010";
+overviewPicture = "\XIM_Core\XIMCore.paa";
