527d9802968d4b3783dd8f97e5f52f4576557709
x-00010 <x00010@criptext.com>
2020-12-27 23:08:51 +0000

    Changed icon name

--- mod.cpp
+++ mod.cpp
@@ -1,12 +1,12 @@
 name = "XIM - Ultimate Music Collection Integration";
-picture = "\XIM_UMC_Integration\XIMCore.paa";
+picture = "\XIM_UMC_Integration\XIMUMC.paa";
 actionName = "Steam";
 action = "placeholder";
 description = "An expansion patch on XIM to include music featured in Hannes' Ultimate Music Collection (UMC) mod into the dynamic system.";
-logo = "\XIM_UMC_Integration\XIMCore.paa";
-logoOver = "\XIM_UMC_Integration\XIMCore.paa";
+logo = "\XIM_UMC_Integration\XIMUMC.paa";
+logoOver = "\XIM_UMC_Integration\XIMUMC.paa";
 tooltip = "XIM - UMC Integration";
 tooltipOwned = "";
 overview = "An expansion patch on XIM to include music featured in Hannes' Ultimate Music Collection (UMC) mod into the dynamic system.";
 author = "H.Finch and x00010";
-overviewPicture = "\XIM_UMC_Integration\XIMCore.paa";
+overviewPicture = "\XIM_UMC_Integration\XIMUMC.paa";
